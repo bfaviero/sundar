20e46fd1ae94fb6d2ecc6c5b7bdaf5480107ab13
kfan <kyangfan@gmail.com>
2013-12-17 02:27:59 -0500

    settings.py edited with RDS info

--- backend/settings.py
+++ backend/settings.py
@@ -57,8 +57,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sundardb',
+        'USER': ‘sundar’,
+        'PASSWORD': 'mAR.20ecWk',
+        'HOST': 'sundar-instance-1.cxpjixe6nt87.us-west-2.rds.amazonaws.com:3306',
+        'PORT': '3306',
     }
 }
 
