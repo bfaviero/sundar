b13770b1e374bef29d61a070224c652c227841de
kfan <kyangfan@gmail.com>
2013-12-18 15:46:09 -0500

    from var/www to var/django

--- wsgi.py
+++ wsgi.py
@@ -16,7 +16,7 @@ framework.
 import os
 
 import sys
-sys.path.insert(0, '/var/www/sundar-backend')
+sys.path.insert(0, '/var/django/sundar-backend')
 
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
