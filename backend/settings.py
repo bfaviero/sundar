558f593488ec6e2f7fb7c1a7548edc074744c569
kfan <kyangfan@gmail.com>
2013-12-19 01:48:58 -0500

    missing import

--- backend/settings.py
+++ backend/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'models'
+    'backend.models'
 )
 
 MIDDLEWARE_CLASSES = (
