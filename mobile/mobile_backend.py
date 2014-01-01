01143306ce9337663483f8a49f1bba2e594ae22d
kfan <kyangfan@gmail.com>
2014-01-01 14:44:35 -0500

    bug

--- mobile/mobile_backend.py
+++ mobile/mobile_backend.py
@@ -2,6 +2,8 @@ from utils import request_arg, optional_request_arg
 from django.http import HttpResponse
 from django.core.context_processors import csrf
 from templating import Template, error_page
+from django.contrib.auth.hashers import make_password, check_passowrd, is_password_usable
+from backend.models import Supplier
 
 def render_login(request):
     context = {}
@@ -11,17 +13,25 @@ def render_login(request):
 def login(request):
     email_addr = request_arg(request, "email_addr")
     password = request_arg(request, "password")
-    return redirect("/main")
+    if email_addr == check_passowrd(password):
+        pass
+    return Template("login.html")
 
-    return Template("login.html", context)
-
-make_password
+def sign_up(request):
+    c_name = request_arg(request, "company_name")
+    email_addr = request_arg(request, "email_addr")
+    password = request_arg(request, "password")
+    if is_password_usable(password):
+        db_pass = make_password(password)
+        Supplier(c_name, email_addr, db_pass)
 
 def render_edit_product(request):
     return Template("edit_product.html")
 
 def render_product_list(request):
-    return Template("product_list.html")
+    context = {}
+    #context.update(csrf(request))
+    return Template("product_list.html", context)
 
 def render_sign_up(request):
     return Template("signup.html")
