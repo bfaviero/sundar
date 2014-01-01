3ade31b1a796a4b4acf19fd70bfb04e79334d765
kfan <kyangfan@gmail.com>
2014-01-01 04:32:45 -0500

    fix

--- sundar/settings.py
+++ sundar/settings.py
@@ -70,8 +70,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    BASE_DIR + '../mobile/templates/',
-    BASE_DIR + '../web/templates/',
+    BASE_DIR + '/../mobile/templates/',
+    BASE_DIR + '/../web/templates/',
 )
 
 MEDIA_URL = '/media/'
@@ -95,7 +95,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    BASE_DIR + '../static/',
+    BASE_DIR + '/../static/',
     #os.path.join(BASE_DIR, "../static/"),
     os.path.join(BASE_DIR, "static"),
 )
