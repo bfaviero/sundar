b39a432d86b741fda421d4d6deb1a9e465b48d15
kfan <kyangfan@gmail.com>
2014-01-02 04:39:26 -0500

    upload  functionality

--- backend/item_api.py
+++ backend/item_api.py
@@ -3,7 +3,8 @@ from utils import request_arg, optional_request_arg as ora
 from django.http import HttpResponse
 from django.core.context_processors import csrf
 from django.models import Supplier, Item
-
+from django.shortcuts import redirect
+from s3_api import upload_image
 def get_item(request):
     item_id = request_arg(request, "item_id")
     return Item.objects.get(id=item_id)
@@ -12,20 +13,38 @@ def set_item(request):
     """ If item exists, update item. Else, create new item """
     item = None
     item_id = ora(request, "item_id")
+    #TODO: supplier = ora(request, "supplier_id")
+    supplier_id = "007"
     if item_id:
-        try: item = Item.objects.get(id=item_id)
-        except:item = Item()
+        try: 
+            item = Item.objects.get(id=item_id)
+        except:
+            item = Item()
     else:
         item = Item()
     item.product_name = ora(request, "product_name")
+    item.supplier = supplier_id
     if ora(request, "in_stock"): item.in_stock = True
     else: item.in_stock = False
     item.wholesale_price = "%s %s" % (ora(request, "wholesale_price"), ora(request, "price_units"))
     item.fabric_width = "%s %s" % (ora(request, "fabric_width"), ora(request, "width_units"))
     item.textile_types = ora(request, "textile_types")
     item.weave_types = ora(request, "weave_types")
+    #TODO set image url image1_url
+    if request.FILES['image']:
+        item = _add_image(request.FILES['image'], supplier_id, item)
     item.save()
+    return redirect("/mobile/product_list")
 
+def _add_image(image, supplier_id, item):
+    url = upload_image(image, supplier_id, item.product_name)
+    if not item.image1_url: item.image1_url = url
+    elif not item.image2_url: item.image2_url = url
+    elif not item.image3_url: item.image3_url = url
+    elif not item.image4_url: item.image4_url = url
+    elif not item.image5_url: item.image5_url = url
+    return item
+ 
 def delete_item(request):
     return HttpResponse("SUCCESS DELETE ITEM")
 
--- backend/s3_api.py
+++ backend/s3_api.py
@@ -47,7 +47,14 @@ def upload_image(image, user_id, item_id):
     bucket_key = Key(bucket)
     time_stamp = datetime.now().strftime("%y%m%d_%H%M%S")
     bucket_key.key = user_id + "." + item_id + "." + time_stamp
-    print 'Uploading with bucket key %s to Amazon S3 bucket %s' % (bucket_key.name.encode('utf-8'), IMAGES_BUCKET_NAME)
-    bucket_key.set_contents_from_filename(image, cb=percent_complete, num_cb=10)
+    #print 'Uploading with bucket key %s to Amazon S3 bucket %s' % (bucket_key.name.encode('utf-8'), IMAGES_BUCKET_NAME)
+    #image_stream = StringIO.StringIO(image.read())
+    #bucket_key.set_contents_from_file(image_stream)
+    bucket_key.set_contents_from_string(image.read(), cb=percent_complete, num_cb=10)
+    bucket_key.set_metadata('Content-Type', 'image/jpeg')
+    bucket_key.set_acl('public-read')
+    #bucket_key.make_public()
+    url = bucket_key.generate_url(expires_in=None, query_auth=False)
     #k.get_contents_to_filename('download.tar.gz')
-    print 'Upload complete.'
+    #print 'Upload complete.'
+    return url
